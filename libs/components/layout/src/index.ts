d59d20daaa55fe1f641992afc5add7afeba873e1
tylerkleeberger <108431027+tylerkleeberger@users.noreply.github.com>
2023-04-28 19:31:16 -0400

    added component layout toolbar

--- libs/components/layout/src/index.ts
+++ libs/components/layout/src/index.ts
@@ -1,1 +1,3 @@
 export * from './lib/components-layout.module';
+
+export * from './lib/toolbar/toolbar.component';
--- libs/components/layout/src/lib/components-layout.module.ts
+++ libs/components/layout/src/lib/components-layout.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ToolbarComponent } from './toolbar/toolbar.component';
 
 @NgModule({
   imports: [CommonModule],
+  declarations: [ToolbarComponent],
+  exports: [ToolbarComponent],
 })
 export class ComponentsLayoutModule {}
